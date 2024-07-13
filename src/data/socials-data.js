4127df2ef7b712fb84ff01ba27dc7b180b185610
tedamami <samsonmamushet3@gmail.com>
2024-07-13 17:15:19 +0300

    edit cv

--- src/data/socials-data.js
+++ src/data/socials-data.js
@@ -1,9 +1,9 @@
 export const socialsData = {
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    medium: 'https://medium.com/@abusaid-bd',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    mail: 'abusaid7388@gmail.com',
+    github: 'https://github.com/samisams1',
+    facebook: 'https://www.facebook.com/samisams/',
+    linkedIn: 'https://www.linkedin.com/in/samson-mamushet/',
+    twitter: 'https://twitter.com/samisams',
+    medium: 'https://medium.com/@forsamisams',
+    stackOverflow: '',
+    mail: 'forsamisams@gmail.com',
 }
