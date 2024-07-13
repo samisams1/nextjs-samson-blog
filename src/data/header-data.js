ea27309437d6611785d825f8d842bbd23824841e
tedamami <samsonmamushet3@gmail.com>
2024-07-13 17:03:15 +0300

    edit cv

--- src/data/header-data.js
+++ src/data/header-data.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am Samson Mamushet, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'https://i.ibb.co/2jzQS79/0I1A1066.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/'
+    resumePdf: 'https://docs.google.com/document/d/1FcFmKdvVbQfQYrH2uNbGM-XJMTF6shRcf6mef-n6MCE/'
 }
