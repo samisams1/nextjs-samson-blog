17e0b6a66e1d4f55bf9f6acf48bd145e133f7b1f
tedamami <samsonmamushet3@gmail.com>
2024-07-18 10:37:27 +0300

    about

--- src/data/socials-data.js
+++ src/data/socials-data.js
@@ -2,7 +2,7 @@ export const socialsData = {
     github: 'https://github.com/samisams1',
     facebook: 'https://www.facebook.com/samisams/',
     linkedIn: 'https://www.linkedin.com/in/samson-mamushet/',
-    twitter: 'https://twitter.com/samisams',
+    twitter: 'https://x.com/MamushetSamson',
     medium: 'https://medium.com/@forsamisams',
     stackOverflow: '',
     mail: 'forsamisams@gmail.com',
